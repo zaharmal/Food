db61b2aae13fa5e279b5c609859d897f60b4be86
zaharmal <zahar.salov.00@mail.ru>
2022-08-11 22:45:52 +0700

    added header, about, work sections

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,11 @@
 $(function(){
 
-  
+  $('.about-slider').slick({
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    prevArrow: '<button type="button" class="slick-prev"><img src="images/about/arrow-left.svg" alt=""></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="images/about/arrow-right.svg" alt=""></button>',
+  });
 
 });
